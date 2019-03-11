74c3e6b9145e4b7458df9aea96b8290c360773c4
Sirus Goorhuis <sirus.goorhuis@MacBook-Pro-van-Sirus.local>
2019-03-11 12:54:05 +0100

    First version of the practice project in Vue.js

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+        <h1>Hello {{ their_name }}, this is an about page</h1>
+        <p>This is just some placeholder text to make the page look less empty, but it's still pretty empty</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data()  {
+    return {
+        their_name: this.$route.params.name
+    }
+  }
+}
+</script>
